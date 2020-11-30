53a4975edb7895d7ffa2ffe0b92263f801c845c0
Xiaokai <61805939+13571815901@users.noreply.github.com>
2020-11-30 09:13:21 +0200

    Add files via upload

---
+++ Helloworld.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def Say_hello(name=None):
+    if name is None:
+        return print("Hello world!")
+    else:
+        return print("Hello,",name)
+
+
+# In[8]:
+
+
+Say_hello("K")
+
+
+# In[ ]:
+
+
+
+
